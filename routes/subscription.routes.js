f24eab70765c73e7467c449a9d11c484fd7438b3
Shripad Dhanshetti <shripad1483@gmail.com>
2025-10-16 21:47:37 +0530

    feat(subscription.controller.js): created sub creation (createSubscription()) controller.

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { authorize } from "../middlewares/auth.middleware.js";
+import { createSubscription } from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
@@ -11,9 +13,7 @@ subscriptionRouter.get("/:id", (req, res) => {
     res.send({title: "GET subscription details"});
 });
 
-subscriptionRouter.post("/", (req, res) => {
-    res.send({title: "CREATE new subscription"});
-});
+subscriptionRouter.post("/", authorize, createSubscription);
 
 subscriptionRouter.put("/:id", (req, res) => {
     res.send({title: "UPDATE a subscription"});
