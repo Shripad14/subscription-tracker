58c9cfc9cb135feec89ea9585cf7d60bcead8c5b
Shripad Dhanshetti <shripad1483@gmail.com>
2025-10-17 21:05:36 +0530

    feat(reminder-workflow): (Not a file name) set up a skeleton for reminder-workflow config.

--- config/env.js
+++ config/env.js
@@ -7,6 +7,7 @@ export const {
     NODE_ENV, 
     DB_URI, 
     JWT_SECRET, JWT_EXPIRES_IN, 
-    ARCJET_ENV, ARCJET_KEY 
+    ARCJET_ENV, ARCJET_KEY,
+    QSTASH_TOKEN, QSTASH_URL 
 } = process.env;
 
